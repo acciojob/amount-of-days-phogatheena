98961dcecac90c29556b7fbda44fc1381e56c6da
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-09 15:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function year(daysOfAYear)
+{
+	if(daysOfAYear%4==0 && daysOfAYear%100!=0)
+	{
+		return 366;
+	}
+	else if(daysOfAYear%400==0)
+	{
+		return 366;
+	}
+	else
+		return 365;
+}
+var daysOfAYear = prompt("enter year");
+alert(year(daysOfAYear));
